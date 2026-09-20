192a44802d0fbc9a3009e968a5ac2b2023e08c5e
ErickMGG06 <erickmaster571@gmail.com>
2026-09-20 02:36:19 -0500

    Codigo de Python Basico

---
+++ Program.py
@@ -0,0 +1,13 @@
+# C#
+# Console.WriteLine("Cual es tu nombre");
+# string nom = Console.ReadLine();
+# Console.WriteLine("Cual es tu edad");
+# int edad = int.parse(Console.ReadLine());
+# Console.WriteLine("Te llamas {nom} y tienes {edad} años");
+
+#Python
+print("Cual es tu nombre: ")
+nom : str = input()
+print("Cual es tu edad")
+edad = int(input())
+print(f"Hola {nom} y tu edad es {edad}")   
